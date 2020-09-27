d64d972739ad35167e181d2a35e88cb2ac3f09ca
Gabriel Rosim <gabriel_rosim@yahoo.com.br>
2020-09-27 11:15:25 -0300

    Adicionando Classe Modelo Aluno

---
+++ src/java/crud/desafio/model/Aluno.java
@@ -0,0 +1,39 @@
+package java.crud.desafio.model;
+
+public class Aluno {
+	private Integer id_aluno;
+	private String nome;
+	private Integer idade;
+
+	protected Aluno(Integer id_aluno, String nome, Integer idade) {
+		super();
+		this.id_aluno = id_aluno;
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public Integer getId_aluno() {
+		return id_aluno;
+	}
+
+	public void setId_aluno(Integer id_aluno) {
+		this.id_aluno = id_aluno;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+}
